600f8f1f34302a09750f4ffbac8dde0a8f48a49d
Fynis96 <C.hearth96@gmail.com>
2023-01-06 18:48:31 -0500

    Commented in Sequlize code for postgres connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+// const { Sequelize } = require('sequelize')
 const app = express()
 const cors = require('cors')
 // const middleware = require('./utils/middleware')
@@ -9,7 +10,21 @@ const cors = require('cors')
 // const logger = require('./utils/logger')
 
 //Connect to Postgres
+//      These are two seperate options for connecting to the Postgres DB, just need to settle on which is needed.
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
 
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+
+//Test DB Connection
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
 
 app.use(cors())
 app.use(express.json())
