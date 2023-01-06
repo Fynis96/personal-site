0d04b44be018b9ba196a61cd8cacb57b4945a091
Fynis96 <C.hearth96@gmail.com>
2023-01-06 18:51:18 -0500

    Added in env vars to config for postgres connection

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
+//const POSTGRES_URI = process.env.POSTGRES_URI
+//const POSTGRES_PORT = process.env.POSTGRES_PORT
+//const POSTGRES_USERNAME = process.env.POSTGRES_USERNAME
+//const POSTGRES_DBNAME = process.env.POSTGRES_DBNAME
+//const POSTGRES_PASSWORD = process.env.POSTGRES_PASSOWRD
 
 module.exports = {
   PORT
